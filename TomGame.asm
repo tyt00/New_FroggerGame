9c19c3cafc03ee032ffe6aef2901aa0dcc2efecf
tyt00 <96821684+tyt00@users.noreply.github.com>
2022-01-13 19:51:57 +0200

    backround_finish1

--- TomGame.asm
+++ TomGame.asm
@@ -3,7 +3,8 @@ MODEL small
 STACK 100h
 DATASEG
 count db 4
-
+count2 db 24
+count3 db 72
 
 Frog  	    db 't', 0Ah, 't', 't', 0Eh, 0Ah, 0Eh, 0Eh, 't', 't', 0Ah, 't', 'n'
 		  	db 0Ah, 0Ah, 't', 0Dh, 0Ah, 0Eh, 0Eh, 0Ah, 0Dh, 't', 0Ah, 0Ah, 'n'
@@ -20,81 +21,138 @@ Frog  	    db 't', 0Ah, 't', 't', 0Eh, 0Ah, 0Eh, 0Eh, 't', 't', 0Ah, 't', 'n'
 		  	db '$'
 CODESEG
 proc sides
-mov cx, 5
-mov dx, 0
-LeftSideL1:
-dec cx
-LeftSideL2:
-mov al,0Fh
-mov ah,0ch
-int 10h
-inc dx
-cmp dx, 200
-jne LeftSideL2
-mov dx, 0
-inc cx
-loop LeftSideL1
+	mov cx, 5
+	mov dx, 0
+	LeftSideL1:
+	dec cx
+	LeftSideL2:
+	mov al,0Fh
+	mov ah,0ch
+	int 10h
+	inc dx
+	cmp dx, 200
+	jne LeftSideL2
+	mov dx, 0
+	inc cx
+	loop LeftSideL1
 
-mov cx, 319
-mov dx, 0
-RightSideL:
-mov al,0Fh
-mov ah,0ch
-int 10h
-inc dx
-cmp dx, 200
-jne RightSideL
-mov dx, 0
-cmp cx, 315
-jne loopRight
-mov cx, 1
-loopRight:
-loop RightSideL
+	mov cx, 319
+	mov dx, 0
+	RightSideL:
+	mov al,0Fh
+	mov ah,0ch
+	int 10h
+	inc dx
+	cmp dx, 200
+	jne RightSideL
+	mov dx, 0
+	cmp cx, 315
+	jne loopRight
+	mov cx, 1
+	loopRight:
+	loop RightSideL
 
-mov cx, 3
-mov dx, 0
-UpL:
-mov al,0Fh
-mov ah,0ch
-int 10h
-inc cx
-cmp cx, 315
-jne UpL
-cmp [count], 0
-je finishUpL
-dec [count]
-inc dx
-mov cx,0
-jmp UpL
+	mov cx, 3
+	mov dx, 0
+	UpL:
+	mov al,0Fh
+	mov ah,0ch
+	int 10h
+	inc cx
+	cmp cx, 315
+	jne UpL
+	cmp [count], 0
+	je finishUpL
+	dec [count]
+	inc dx
+	mov cx,0
+	jmp UpL
 
-finishUpL:
-mov [count], 4
-mov cx, 3
-mov dx, 199
-DownL:
-mov al,0Fh
-mov ah,0ch
-int 10h
-inc cx
-cmp cx, 315
-jne DownL
-cmp [count], 0
-je finishDownL
-dec [count]
-dec dx
-mov cx, 0 
-jmp DownL
+	finishUpL:
+	mov [count], 4
+	mov cx, 3
+	mov dx, 199
+	DownL:
+	mov al,0Fh
+	mov ah,0ch
+	int 10h
+	inc cx
+	cmp cx, 315
+	jne DownL
+	cmp [count], 0
+	je finishDownL
+	dec [count]
+	dec dx
+	mov cx, 0 
+	jmp DownL
 
-finishDownL:
-ret
+	finishDownL:
+	ret
 endp sides
+
+proc backround
+	mov cx, 5
+	mov dx, 5
+	EndL:
+	mov al,08h
+	mov ah,0ch
+	int 10h
+	inc cx
+	cmp cx, 315
+	jne EndL
+	cmp [count2], 0
+	je finishEndL
+	dec [count2]
+	inc dx
+	mov cx,5
+	jmp EndL
+	finishEndL:
+	
+	mov [count2], 24
+	mov cx, 5
+	mov dx, 194
+	StartL:
+	mov al,08h
+	mov ah,0ch
+	int 10h
+	inc cx
+	cmp cx, 315
+	jne StartL
+	cmp [count2], 0
+	je finishStartL
+	dec [count2]
+	dec dx
+	mov cx, 5 
+	jmp StartL
+	finishStartL:
+
+	mov cx, 5
+	mov dx, 30
+	River:
+	mov al,01h
+	mov ah,0ch
+	int 10h
+	inc cx
+	cmp cx, 315
+	jne River
+	cmp [count3], 0
+	je finishRiver
+	dec [count3]
+	inc dx
+	mov cx, 5 
+	jmp River
+	finishRiver:
+ret
+endp backround
 start :
 ; Graphic mode
     mov ax, @data
     mov ds, ax
     mov ax, 13h
     int 10h
+
     call sides
+	call backround
     mov ah, 00h
     int 16h
 
