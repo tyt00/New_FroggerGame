a09a08ff3db3b2c9eaf3609e7b49c926eb6dc41c
tyt00 <96821684+tyt00@users.noreply.github.com>
2022-01-13 21:36:17 +0200

    finish_frog

--- TomGame.asm
+++ TomGame.asm
@@ -5,6 +5,7 @@ DATASEG
 count db 4
 count2 db 24
 count3 db 72
+count4 db 2
 
 Frog  	    db 't', 0Ah, 't', 't', 0Eh, 0Ah, 0Eh, 0Eh, 't', 't', 0Ah, 't', 'n'
 		  	db 0Ah, 0Ah, 't', 0Dh, 0Ah, 0Eh, 0Eh, 0Ah, 0Dh, 't', 0Ah, 0Ah, 'n'
@@ -20,7 +21,7 @@ Frog  	    db 't', 0Ah, 't', 't', 0Eh, 0Ah, 0Eh, 0Eh, 't', 't', 0Ah, 't', 'n'
 		  	db 't', 0Ah, 't', 't', 't', 't', 't', 't', 't', 't', 0Ah, 't', 'n'
 		  	db '$'
 CODESEG
-proc sides
+proc Sides
 	mov cx, 5
 	mov dx, 0
 	LeftSideL1:
@@ -88,9 +89,9 @@ proc sides
 
 	finishDownL:
 	ret
-endp sides
+endp Sides
 
-proc backround
+proc Backround
 	mov cx, 5
 	mov dx, 5
 	EndL:
@@ -142,19 +143,76 @@ proc backround
 	mov cx, 5 
 	jmp River
 	finishRiver:
-ret
-endp backround
-start :
+	ret
+endp Backround
+
+proc Create_Frog
+	mov cx, 147
+	mov dx, 170
+	mov bx, offset Frog
+
+	Start_Create_Frog:
+	mov al, [bx]
+	cmp al, '$'
+	je Finish_Create_Frog
+
+	cmp al, 't'
+	jne past_t
+	add cx, 2
+	inc bx
+	jmp Start_Create_Frog
+
+	past_t:
+	cmp al, 'n'
+	jne pass_n
+	add dx, 2
+	mov cx, 147
+	inc bx
+	jmp Start_Create_Frog
+
+	pass_n:
+
+	mov [count4], 2
+	xpixel:
+	inc cx
+	mov ah,0ch
+	int 10h
+	dec [count4]
+	cmp [count4], 0
+	jne xpixel
+
+	inc dx
+	mov [count4], 2
+	sub cx,2
+	ypixel:
+	inc cx
+	mov ah,0ch
+	int 10h
+	dec [count4]
+	cmp [count4], 0
+	jne ypixel
+	
+
+	inc bx
+	dec dx
+	jmp Start_Create_Frog
+
+	Finish_Create_Frog:
+	ret
+endp Create_Frog
+start:
 ; Graphic mode
     mov ax, @data
     mov ds, ax
     mov ax, 13h
     int 10h
 
-    call sides
-	call backround
-    mov ah, 00h
+    call Sides
+	call Backround
+	call Create_Frog
+	mov ah, 00h
     int 16h
+    
 
 
 exit :
